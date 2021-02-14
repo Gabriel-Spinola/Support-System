6794f617386497efce71c43115bcc1baaa8bb755
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-02-14 15:56:20 -0300

    Add Home Content, Create its model

--- App/Views/Pages/Home.php
+++ App/Views/Pages/Home.php
@@ -1,1 +1,11 @@
 <h2>Hallo World</h2>
+
+<form method="post">
+    <input type="email" name="email" placeholder="Email...">
+    <textarea name="message" placeholder="Message..."></textarea> <br>
+    <input type="submit" name="submit">
+</form>
+
+<?php
+
+if($_POST['submit']))
