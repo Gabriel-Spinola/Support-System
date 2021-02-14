8213254a734bd4cf1f547c42ecfd6ec7d69be034
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-02-14 15:01:20 -0300

    AutoLoad

--- App/index.php
+++ App/index.php
@@ -6,3 +6,11 @@ const HOST = 'localhost';
 const DATABASE = 'db_support_system';
 const USER = 'root';
 const PASSWORD = '';
+
+// ---------------------------------------------------------
+// Autoload
+$autoload = function(string $className): void {
+    require $className . '.php';
+};
+
+spl_autoload_register($autoload);
