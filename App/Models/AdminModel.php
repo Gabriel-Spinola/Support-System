951722535af4762086cda9671d765c650fe258d8
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-02-14 20:51:37 -0300

    Get Calls Function

--- App/Models/AdminModel.php
+++ App/Models/AdminModel.php
@@ -5,13 +5,17 @@ namespace Models;
 use DBConnectionI;
 
 class AdminModel {
+    private string | int $token;
+
     public function __construct(
         private DBConnectionI $pdo
-    ) {}
+    ) {
+        $this -> token = $_GET['token'] ?? '';
+    }
 
     public function getCalls() {
         $query = $this -> pdo -> connect() -> prepare(
-           "SELECT * FROM `tb_call_answer`
+           "SELECT * FROM `tb_calls`
             ORDER BY id DESC;"
         );
 
@@ -21,10 +25,30 @@ class AdminModel {
 
         foreach ($data as $key => $row): ?>
 
+            <?php
+            
+                $isAnswered = $this -> pdo -> connect() -> prepare(
+                   "SELECT * FROM `tb_call_answer`
+                    WHERE call_id = ?;"
+                );
+
+                $isAnswered -> execute([
+                    $this -> token,
+                ]);
+
+                if ($isAnswered -> rowCount() >= 1)
+                    continue;
+            ?>
+
+            <hr>
+            <h4><?php print $row['message'] ?></h4>
+
             <form method="POST">
             
-                <textarea></textarea>
-                <input type="submit" name="submit">
+                <textarea name="message" placeholder="Your Answer..."></textarea> <br>
+                <input type="submit" name="new-call-submit" value="Reply!">
+                <input type="hidden" name="token" value="<?php print $row['token'] ?>">
+                <input type="hidden" name="email" value="<?php print $row['email'] ?>">
 
             </form>
 
