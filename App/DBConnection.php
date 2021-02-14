58ccc5d559203ecd9cdd620c07e5a0d65e22bfb4
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-02-14 14:58:45 -0300

    Create Database Connection Class

---
+++ App/DBConnection.php
@@ -0,0 +1,25 @@
+<?php
+
+interface DBConnectionI {
+    public function connect(): PDO;
+}
+
+class MySql extends DBConnectionI {
+    private PDO $pdo;
+
+    public function connect(): PDO {
+        if ($this -> pdo == null) {
+            try {
+                $this -> pdo = new PDO('mysql:host' . HOST . ';dbname=' . DATABASE, USER, PASSWORD, [
+                    PDO :: MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+                ]);
+
+                $this -> pdo -> setAttribute(PDO :: ATTR_ERRMODE, PDO :: ERRMODE_EXCEPTION);
+            } catch(Exception $e) {
+                echo 'PDO::ERROR::"Database Connection Failure"';
+            }
+        }
+
+        return $this -> pdo;
+    }
+}
--- App/index.php
+++ App/index.php
@@ -1,2 +1,8 @@
 <?php
 
+// ---------------------------------------------------------
+// Constants
+const HOST = 'localhost';
+const DATABASE = 'db_support_system';
+const USER = 'root';
+const PASSWORD = '';
