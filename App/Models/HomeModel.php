c71febc6f1a0cf7ef4f59fe262e7f570ed2494d0
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-02-17 13:52:29 -0300

    Update HomeModel.php

--- App/Models/HomeModel.php
+++ App/Models/HomeModel.php
@@ -5,12 +5,24 @@ namespace Models;
 use DBConnectionI;
 use EmailSendingI;
 
+/**
+ * @namespace Controllers
+ * @Receive $view, $model, execute(): void
+ * @From Controller
+ * 
+ * @Use DataBaseConnection, EmailSending
+*/
 class HomeModel {
     public function __construct(
         private DBConnectionI $pdo,
         private EmailSendingI $email,
     ) {}
     
+    /**
+     * - Set email address
+     * - Format the email
+     * - Try to send it, if works returns true else returns false
+    */
     public function sendEmail(string $email, string | int $token): bool {
         $this -> email -> AddAddress($email, 'Gabriel');
         
@@ -23,6 +35,9 @@ class HomeModel {
         return $this -> email -> SendEmail();
     }
 
+    /**
+     * Send The initial call form 
+    */
     public function sendForm(string $email, string $message, string | int $token): bool {
         $query = $this -> pdo -> connect() -> prepare(
            "INSERT INTO `tb_calls`
