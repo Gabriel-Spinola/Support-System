dbe7d0e1925f7df1d75e90f32d1536065eb22c0b
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-02-17 13:09:37 -0300

    Update AdminController.php

--- App/Controllers/AdminController.php
+++ App/Controllers/AdminController.php
@@ -4,7 +4,19 @@ namespace Controllers;
 
 use Views\MainView;
 
+/**
+ * @namespace Controllers
+ * @Receive $view, $model, execute(): void
+ * @From Controller
+*/
 class AdminController extends Controller {
+    /**
+     *### **Send page to the client With custom info**
+     * 
+     * @return Page-View
+     * 
+     * - Rendering
+    */
     public function execute(): void {
         $this -> view = new MainView('Admin');
         $this -> view -> render();
